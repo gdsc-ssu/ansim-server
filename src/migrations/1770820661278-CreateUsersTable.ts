c61545c835ea048ce3cebfc0f600cb7f8e99ec30
Hyerin Park <phl2510@naver.com>
2026-02-21 13:52:27 +0900

    feat: User 테이블 마이그레이션 파일 수정

---
+++ src/migrations/1770820661278-CreateUsersTable.ts
@@ -0,0 +1,25 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateUsersTable1770820661278 implements MigrationInterface {
+  name = 'CreateUsersTable1770820661278';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      CREATE TABLE "users" (
+        "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+        "googleId" character varying NOT NULL,
+        "email" character varying NOT NULL,
+        "name" character varying,
+        "profileImage" character varying,
+        "createdAt" TIMESTAMP NOT NULL DEFAULT now(),
+        CONSTRAINT "UQ_users_googleId" UNIQUE ("googleId"),
+        CONSTRAINT "UQ_users_email" UNIQUE ("email"),
+        CONSTRAINT "PK_users" PRIMARY KEY ("id")
+      )
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "users"`);
+  }
+}
