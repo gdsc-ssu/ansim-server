c2a58ce69ecc7ad37a63591d5d517794e8bca5f2
joojae02 <jake010417@gmail.com>
2026-03-03 20:46:16 +0900

    feat: add example scheduler

--- src/scheduler/scheduler.module.ts
+++ src/scheduler/scheduler.module.ts
@@ -1,6 +1,7 @@
 import { Global, Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { DistributedCronService } from './distributed-cron.service';
+import { ExampleHealthScheduler } from './example-health.scheduler';
 import { InMemoryLockService } from './in-memory-lock.service';
 import { DISTRIBUTED_LOCK } from './scheduler.constants';
 
@@ -13,6 +14,7 @@ import { DISTRIBUTED_LOCK } from './scheduler.constants';
       useClass: InMemoryLockService,
     },
     DistributedCronService,
+    ExampleHealthScheduler,
   ],
   exports: [DistributedCronService],
 })
